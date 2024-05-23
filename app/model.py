17f15f7e72445e578ea4e5cd9508edec559534b0
Yu-Ting, Chou <r10941128@g.ntu.edu.tw>
2024-05-23 13:32:11 +0800

    update #2 commit

--- app/model.py
+++ app/model.py
@@ -1,11 +1,11 @@
 # -*- coding: UTF-8 -*-
-from joblib import dump, load
+from joblib import load
 import pandas as pd
 # 載入模型
 RandomForestModel = load('./app/model/best_random_forest_model_4.joblib')
 
 def predict(input):
-    pred = RandomForestModel.predict(input)[0]
     input_df = pd.DataFrame(input, columns=['CTR', 'KTE_kurt(IR)', 'SE_mean', 'KTE_skew(IR)'])
+    pred = RandomForestModel.predict(input_df)[0]
     print(pred)
     return pred
